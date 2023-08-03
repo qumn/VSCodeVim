d84868e372d811b95350f75a652fc9feaba4c099
qumn <wang247657@163.com>
2023-08-03 23:05:53 +0800

    remove the surround default map

--- src/actions/plugins/pluginDefaultMappings.ts
+++ src/actions/plugins/pluginDefaultMappings.ts
@@ -6,29 +6,7 @@ export class PluginDefaultMappings {
     mode: string;
     configSwitch: string;
     mapping: IKeyRemapping;
-  }> = [
-    // default maps for surround
-    {
-      mode: 'normalModeKeyBindingsNonRecursive',
-      configSwitch: 'surround',
-      mapping: { before: ['y', 's'], after: ['<plugys>'] },
-    },
-    {
-      mode: 'normalModeKeyBindingsNonRecursive',
-      configSwitch: 'surround',
-      mapping: { before: ['y', 's', 's'], after: ['<plugys>', '<plugys>'] },
-    },
-    {
-      mode: 'normalModeKeyBindingsNonRecursive',
-      configSwitch: 'surround',
-      mapping: { before: ['c', 's'], after: ['<plugcs>'] },
-    },
-    {
-      mode: 'normalModeKeyBindingsNonRecursive',
-      configSwitch: 'surround',
-      mapping: { before: ['d', 's'], after: ['<plugds>'] },
-    },
-  ];
+  }> = [];
 
   public static getPluginDefaultMappings(mode: string, config: IConfiguration): IKeyRemapping[] {
     return this.defaultMappings
